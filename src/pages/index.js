75de7fe3c745a6b6b0bdad33e5e4a975b223ce79
iletenoux <seedspace6498@gmail.com>
2024-08-13 02:20:23 +0400

    added cdn videos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
 import * as React from "react"
+import { AdvancedVideo } from '@cloudinary/react';
+import {Cloudinary} from "@cloudinary/url-gen";
 import logo from "../images/icon.png"
 import portrait from "../images/portrait.jpeg"
 
 const IndexPage = () => {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'dracbyle6'
+    }
+  });
+
+  const myImage = cld.image('sample');
+
   return (
     <main className="p-10">
       <nav className="flex justify-between">
@@ -22,7 +32,26 @@ const IndexPage = () => {
         className="rounded-t-full rounded-r-full"
       />TIVE LISA
       </h1>
+      
+      <div className="flex gap-10">
 
+      <AdvancedVideo 
+      width="240"
+      cldVid={cld.video('iripoliri/uos8eykf2ujhkfbp1o47').quality('auto')}
+      controls
+      />
+      <AdvancedVideo 
+      width="240"
+      cldVid={cld.video('iripoliri/mdba1lecwe4dkwdjswgx').quality('auto')}
+      controls
+      />
+      <AdvancedVideo 
+      width="240"
+      cldVid={cld.video('iripoliri/uos8eykf2ujhkfbp1o47').quality('auto')}
+      controls
+      />
+      </div>
+    
     </main>
   )
 }
